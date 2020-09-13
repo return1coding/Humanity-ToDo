ec12e4c518bc08a2a37ec1d935e4a8cc1a5833d7
Justine Chang <return1coding@gmail.com>
2020-09-14 05:25:39 +0800

    tried mongoose, didn't work, try doing it on server my dude

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,23 +1,32 @@
 import React, { Component } from 'react';
 import ToDoListCard from './ToDoListCard';
+const mongoose = require('mongoose');
 
+// //connect to MongoDB
+// const dbURI = 'mongodb+srv://humanitytodo:Jostono94mongodb@cluster0.ec2ig.gcp.mongodb.net/humanity-todo?retryWrites=true&w=majority';
+// mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+//         .then((result) => console.log("connected to db"))
+//         .catch((err) => console.log(err));
 export default class ToDoList extends Component {
     render() {
         return (
             <div className="actualList">
-                <ToDoListCard/>
-                <ToDoListCard/>
-                <ToDoListCard/>
-                <ToDoListCard/>
-                <ToDoListCard/>
-                <ToDoListCard/>
-                <ToDoListCard/>
-                <ToDoListCard/>
-                <ToDoListCard/>
-                <ToDoListCard/>
-                <ToDoListCard/>
-                <ToDoListCard/>
-                <ToDoListCard/>         
+                <ToDoListCard title="hellothere" location="Malaysia" time="Dec 32, 2020" />
+                <ToDoListCard title="hellothere" location="Malaysia" time="Dec 32, 2020" />
+                <ToDoListCard title="hellothere" location="Malaysia" time="Dec 32, 2020" />
+                <ToDoListCard title="hellothere" location="Malaysia" time="Dec 32, 2020" />
+                <ToDoListCard title="hellothere" location="Malaysia" time="Dec 32, 2020" />
+                <ToDoListCard title="hellothere" location="Malaysia" time="Dec 32, 2020" />
+                <ToDoListCard title="hellothere" location="Malaysia" time="Dec 32, 2020" />
+                <ToDoListCard title="hellothere" location="Malaysia" time="Dec 32, 2020" />
+                <ToDoListCard title="hellothere" location="Malaysia" time="Dec 32, 2020" />
+                <ToDoListCard title="hellothere" location="Malaysia" time="Dec 32, 2020" />
+                <ToDoListCard title="hellothere" location="Malaysia" time="Dec 32, 2020" />
+                <ToDoListCard title="hellothere" location="Malaysia" time="Dec 32, 2020" />
+                <ToDoListCard title="hellothere" location="Malaysia" time="Dec 32, 2020" />
+                <ToDoListCard title="hellothere" location="Malaysia" time="Dec 32, 2020" />
+                <ToDoListCard title="hellothere" location="Malaysia" time="Dec 32, 2020" />
+                <ToDoListCard title="hellothere" location="Malaysia" time="Dec 32, 2020" />
             </div>
         )
     }
