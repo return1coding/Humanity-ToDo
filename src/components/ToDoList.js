9e6aa9aed4b1b4db65d9f279f49c9b5616c35ed5
return1 <return1coding@gmail.com>
2020-11-04 23:23:37 +0800

    Finally using functional components for todo list part, API call to Mongo is working fine. Now just need to add the "ADD" todo part.

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import ToDoListCard from './ToDoListCard';
 const mongoose = require('mongoose');
 
-// var listOfToDos = [];
+var listOfToDos = [];
 
 // fetch('https://justinevm.ddns.net/get-all-todos')
 //     .then(response => response.json())
@@ -10,35 +10,70 @@ const mongoose = require('mongoose');
 //         listOfToDos.push(data);
 //     });
 
+const ToDoList = () => {
+    const [todos, setTodos] = useState([]);
 
-
-
-export default class ToDoList extends Component {
-
-    constructor() {
-        super();
-        this.state = { data: [] };
+    const fetchTodos = async () => {
+         const apiCall = await fetch('https://justinevm.ddns.net/get-all-todos');
+         const todos = await apiCall.json();
+         setTodos(todos.result);
+         console.log(todos);
     }
-
-    async componentDidMount() {
-        const response = await fetch(`https://justinevm.ddns.net/get-all-todos`);
-        console.log(response);
-        const json = await response.json();
-        console.log(json);
-        this.setState({ data: json });
-    }
-
-    render() {
-        return (
-            <div className="actualList">
-                {this.state.data.map(function (d, idx) {
-                    return <ToDoListCard
-                        title={(d.title).toLowerCase()}
-                        location={(d.from).toLowerCase()}
-                        time={d.time}
-                    />
-                })}
-            </div>
-        )
-    }
+    useEffect(() => {
+        fetchTodos();
+    }, [])
+    return (
+        <div className = "actualList">
+            <ul>
+                {
+                    todos.map(todo => {
+                        return (
+                            <ToDoListCard
+                                title={(todo.title).toLowerCase()}
+                                location={(todo.from).toLowerCase()}
+                                time={todo.time}
+                            />
+                        )
+                    })
+                }
+            </ul>
+        </div>
+    )
 }
+
+export default ToDoList;
+
+// export default class ToDoList extends Component {
+
+//     state = {
+//         data: [],
+//       };
+
+//     constructor() {
+//         super();
+//     }
+
+//     async componentDidMount() {
+//         const response = await fetch(`https://justinevm.ddns.net/get-all-todos`);
+//         const json = await response.json();
+//         // console.log(json)
+//         // this.setState({ data: json });
+//     }
+
+//     render() {
+//         return (
+//             // <div>hello</div>
+//             <div className="actualList">
+//                 hello
+//                 {/* {this.state.data.result.map(function (d, idx) {
+//                     return <ToDoListCard
+//                         title={(d.title).toLowerCase()}
+//                         location={(d.from).toLowerCase()}
+//                         time={d.time}
+//                     />
+//                 })} */}
+//             </div>
+
+//         )
+//     }
+// }
