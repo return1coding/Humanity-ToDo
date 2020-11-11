3f75553f0aab23cd80637db10be3f6083a04a53f
return1 <return1coding@gmail.com>
2020-11-11 23:02:47 +0800

    More work to be done on this, need to use a context to call setState of todolist component after submission to mongodb

--- src/components/FormInputArea.js
+++ src/components/FormInputArea.js
@@ -1,42 +1,65 @@
-import React, { Component } from 'react'
+import React, { Component, useState, useEffect } from 'react'
 
-export default class FormInputArea extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            todo: '',
-        };
+
+const FormInputArea = () => {
+    const [todos, setTodos] = useState([]);
+
+    const fetchTodos = async () => {
+         const apiCall = await fetch('https://justinevm.ddns.net/get-all-todos');
+         const todos = await apiCall.json();
+         setTodos(todos.result);
+         console.log(todos);
     }
-
-    handleChange = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
-
-    onSubmit = async (e) => {
-        const form = {
-            title: this.state.todo,
-        }
-        await fetch('https://justinevm.ddns.net/submit-todo', {
-            method: 'POST',
-            body: JSON.stringify(form),
-            headers: {
-                'Content-Type': 'application/json'
-                // 'Content-Type': 'application/x-www-form-urlencoded',
-            },
-        });
-        this.setState({
-            todo: ''
-        })
-    }
-
-    render() {
-        return (
+    useEffect(() => {
+        fetchTodos();
+    }, [])
+    return (
             <form className="form">
                 <input placeholder="what are we missing?" className="Input-text" name="todo" onChange={e => this.handleChange(e)}></input>
                 <button type="button" className="submitButton" onClick={(e) => { this.onSubmit(e); }}>+</button>
             </form>
         )
-    }
 }
+
+export default FormInputArea;
+
+// export default class FormInputArea extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             todo: '',
+//         };
+//     }
+
+//     handleChange = (e) => {
+//         this.setState({
+//             [e.target.name]: e.target.value
+//         })
+//     }
+
+//     onSubmit = async (e) => {
+//         const form = {
+//             title: this.state.todo,
+//         }
+//         await fetch('https://justinevm.ddns.net/submit-todo', {
+//             method: 'POST',
+//             body: JSON.stringify(form),
+//             headers: {
+//                 'Content-Type': 'application/json'
+//                 // 'Content-Type': 'application/x-www-form-urlencoded',
+//             },
+//         });
+//         this.setState({
+//             todo: ''
+//         })
+//     }
+
+//     render() {
+//         return (
+//             <form className="form">
+//                 <input placeholder="what are we missing?" className="Input-text" name="todo" onChange={e => this.handleChange(e)}></input>
+//                 <button type="button" className="submitButton" onClick={(e) => { this.onSubmit(e); }}>+</button>
+//             </form>
+//         )
+//     }
+// }
